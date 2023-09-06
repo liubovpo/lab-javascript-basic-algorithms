f0445633a2c2b39370f9efe03b00b75f81366dff
liubovpo <102779079+liubovpo@users.noreply.github.com>
2023-09-06 12:19:42 +0200

    main tasks

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
 // Iteration 1: Names and Input
+let hacker1 = "Liubov"
+console.log(`The driver's name is ${hacker1}`)
 
+let hacker2 = "Alice"
+console.log(`The navigator's name is  ${hacker2}`)
 
 // Iteration 2: Conditionals
 
+if(hacker1.length > hacker2.length){
+  
+    console.log(`The driver has the longest name, it has ${hacker1.length} characters.`)
+  } else if(hacker1.length < hacker2.length){
+    console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`)
+  } else if (hacker1.length === hacker2.length) {
+    console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`)
+  } else{
+    console.log("Something went wrong")
+  }
 
 // Iteration 3: Loops
+
+let nameWithSpaces = ""
+for (let i = 0; i < hacker1.length; i++){ 
+  nameWithSpaces += hacker1[i] + " "  
+}
+
+console.log(nameWithSpaces.toUpperCase())
+
+let nameReverse = ""
+
+for (let i = hacker2.length-1; i >= 0; i-- ){
+  
+  nameReverse += hacker2[i]
+}
+console.log(nameReverse)
